13928ef138fea9ec91bace0f94e588933d2dd14f
Kalman Szenes <szenes.kalman@gmail.com>
2025-02-16 01:08:43 +0100

    refactor

--- src/h5tui/h5tui.py
+++ src/h5tui/h5tui.py
@@ -1,11 +1,12 @@
 from textual.app import App, ComposeResult
-from textual.widget import Widget
 from textual.widgets import Footer, Header, OptionList, Static
-from textual.containers import VerticalScroll, Horizontal
+from textual.containers import VerticalScroll, Horizontal, Container
 from textual.binding import Binding
 
 import h5py
+import numpy as np
 
+import sys
 import os
 import argparse
 
@@ -14,38 +15,64 @@ class MyOptionList(OptionList):
     BINDINGS = [
         Binding("down,j", "cursor_down", "Down", show=True),
         Binding("up,k", "cursor_up", "Up", show=True),
-        Binding("G", "page_down", "Page down", show=False),
-        Binding("g", "page_up", "Page up", show=False),
+        Binding("G", "page_down", "Bottom", show=False),
+        Binding("g", "page_up", "Top", show=False),
     ]
 
 
+# TODO: This shoudln't be a VerticalScroll, just a widget and the
+#       content should be a VerticalScroll, breaks the h keybinding for some reason
 class ColumnContent(VerticalScroll):
     """Column which displays a dataset"""
 
+    BINDINGS = [
+        Binding("down,j", "scroll_down", "Down", show=True),
+        Binding("up,k", "scroll_up", "Up", show=True),
+        Binding("G", "scroll_end", "Bottom", show=False),
+        Binding("g", "scroll_home", "Top", show=False),
+        Binding("t", "truncate_print", "Truncate print", show=False),
+    ]
+
+    def __init__(self, id):
+        super().__init__(id=id)
+        self.truncate_print = True
+
     def compose(self):
-        self._header = Static()
+        self._header = Static(classes="header")
         self._content = Static()
 
         yield self._header
         yield self._content
 
     def update(self, path, name, shape, value):
+        # save value to be able to call it in toggle truncate
+        self._value = value
+
         self._header.update(f"Path: {path}\nDataset: {name} {shape}")
-        self._content.update(f"{value}")
+        self._content.update(f"{self._value}")
+
+    def action_truncate_print(self):
+        self.truncate_print = not self.truncate_print
+        if self.truncate_print:
+            default_numpy_truncate = 1000
+            np.set_printoptions(threshold=default_numpy_truncate)
+        else:
+            np.set_printoptions(threshold=sys.maxsize)
+
+        self._content.update(f"{self._value}")
 
 
-class ColumnOption(Widget):
+class ColumnOption(Container):
     """Column which shows directory structure and selector"""
 
     BINDINGS = [
-        ("left,h", "goto_parent", "Parent Directory"),
-        ("right,l", "goto_child", "Select"),
+        Binding("left,h", "goto_parent", "Parent Directory", show=True),
+        Binding("right,l", "goto_child", "Select", show=True),
     ]
 
     def __init__(self, fname: h5py.File, focus=False):
         super().__init__()
         self._focus = focus
-        self._dir_hidden = False
 
         self._prev_highlighted = 0
 
@@ -56,14 +83,14 @@ class ColumnOption(Widget):
 
     def compose(self):
         if self._dirs:
-            self._path_widget = Static(f"Path: {self._cur_dir}")
-            self._list_widget = MyOptionList(*self._dirs, id="dirs")
+            self._header_widget = Static(f"Path: {self._cur_dir}", classes="header")
+            self._selector_widget = MyOptionList(*self._dirs, id="dirs")
             self._content_widget = ColumnContent(id="content")
-            yield self._path_widget
-            yield self._list_widget
+            yield self._header_widget
+            yield self._selector_widget
             yield self._content_widget
             if self._focus:
-                self._list_widget.focus()
+                self._selector_widget.focus()
 
     def get_dir_content(self, dir) -> list[str]:
         """Return contents of current path"""
@@ -71,38 +98,36 @@ class ColumnOption(Widget):
 
     def update_list(self):
         """Redraw option list with contents of current directory"""
-        self._list_widget.clear_options()
+        self._selector_widget.clear_options()
         self._dirs = self.get_dir_content(self._cur_dir)
-        self._list_widget.add_options(self._dirs)
-        self._list_widget.highlighted = self._prev_highlighted
+        self._selector_widget.add_options(self._dirs)
+        self._selector_widget.highlighted = self._prev_highlighted
 
     def action_goto_parent(self) -> None:
         """Either displays parent or hides dataset"""
         has_parent_dir = self._cur_dir != "/"
-        if has_parent_dir and not self._dir_hidden:
+        if has_parent_dir and not self.has_class("view-dataset"):
             self._cur_dir = os.path.dirname(self._cur_dir)
-            self._path_widget.update(f"Path: {self._cur_dir}")
+            self._header_widget.update(f"Path: {self._cur_dir}")
             self.update_list()
         self.remove_class("view-dataset")
-        self._dir_hidden = False
 
     def action_goto_child(self) -> None:
         """Either displays child or dataset"""
-        highlighted = self._list_widget.highlighted
+        highlighted = self._selector_widget.highlighted
         if highlighted is not None:
             path = os.path.join(
                 self._cur_dir,
-                str(self._list_widget.get_option_at_index(highlighted).prompt),
+                str(self._selector_widget.get_option_at_index(highlighted).prompt),
             )
             if path in self._file:
                 if isinstance(self._file[path], h5py.Group):
                     self._prev_highlighted = highlighted
                     self._cur_dir = path
-                    self._path_widget.update(f"Path: {self._cur_dir}")
+                    self._header_widget.update(f"Path: {self._cur_dir}")
                     self.update_list()
                 else:
                     self.add_class("view-dataset")
-                    self._dir_hidden = True
 
                     dset = self._file[path]
                     dset_name = os.path.basename(path)
@@ -119,6 +144,7 @@ class H5TUIApp(App):
 
     BINDINGS = [("d", "toggle_dark", "Toggle dark mode"), ("q", "quit", "Quit")]
     CSS_PATH = "h5tui.tcss"
+    TITLE = "h5tui"
 
     def __init__(self, h5file):
         super().__init__()
@@ -126,7 +152,7 @@ class H5TUIApp(App):
         self._h5file = h5file
 
     def compose(self) -> ComposeResult:
-        yield Header(name="H5TUI")
+        yield Header()
         yield Footer()
 
         with Horizontal():
